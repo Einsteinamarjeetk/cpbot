e7fb5d98d5c4c6856067558cdbaa13f1ac33f740
Vivekfhfd <132126550+Vivekfhfd@users.noreply.github.com>
2024-02-06 15:19:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ import sys
 import re
 import requests
 
+# Setting up logging
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.INFO,
@@ -33,74 +34,73 @@ logging.basicConfig(
     ],
 )
 
+# Initialize the bot
 bot = Client(
     "bot",
-    api_id= 22779671,
-    api_hash= "125d8d88b77309dc3b154cbbfc2dacb2",    
-    bot_token= "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
+    api_id=api_id,
+    api_hash=api_hash,    
+    bot_token=bot_token
 )
 
+# Handler for /start command
 @bot.on_message(filters.command(["start"]))
 async def start(bot, update):
-       await update.reply_text("Hi i am **Classplus txt Downloader**.\n\n"
-                              "**NOW:-** "
-                                       
-                                       "Press **/classplus** to continue..\n\n")
+    await update.reply_text("Hi, I am **Classplus txt Downloader**.\n\n"
+                            "**NOW:-** "
+                            "Press **/classplus** to continue..\n\n")
 
+# Handler for /classplus command
 @bot.on_message(filters.command(["classplus"]))
 async def account_login(bot: Client, m: Message):
+    try:
+        async def get_course_content(session, course_id, folder_id=0):
+            fetched_contents = []
 
-    def get_course_content(session, course_id, folder_id=0):
+            params = {
+                'courseId': course_id,
+                'folderId': folder_id,
+            }
 
-        fetched_contents = []
+            res = session.get(f'{api}/course/content/get', params=params)
 
-        params = {
-            'courseId': course_id,
-            'folderId': folder_id,
+            if res.status_code == 200:
+                res = res.json()
+                contents = res['data']['courseContent']
+
+                for content in contents:
+                    if content['contentType'] == 1:
+                        resources = content['resources']
+
+                        if resources['videos'] or resources['files']:
+                            sub_contents = await get_course_content(session, course_id, content['id'])
+                            fetched_contents += sub_contents
+
+                    else:
+                        name = content['name']
+                        url = content['url']
+                        fetched_contents.append(f'{name}: {url}')
+
+            return fetched_contents
+
+        headers = {
+            'accept-encoding': 'gzip',
+            'accept-language': 'EN',
+            'api-version': '35',
+            'app-version': '1.4.73.2',
+            'build-number': '35',
+            'connection': 'Keep-Alive',
+            'content-type': 'application/json',
+            'device-details': 'Xiaomi_Redmi 7_SDK-32',
+            'device-id': 'c28d3cb16bbdac01',
+            'host': 'api.classplusapp.com',
+            'region': 'IN',
+            'user-agent': 'Mobile-Android',
+            'webengage-luid': '00000187-6fe4-5d41-a530-26186858be4c'
         }
 
-        res = session.get(f'{api}/course/content/get', params=params)
+        api = 'https://api.classplusapp.com/v2'
 
-        if res.status_code == 200:
-            res = res.json()
-
-            contents = res['data']['courseContent']
-
-            for content in contents:
-
-                if content['contentType'] == 1:
-                    resources = content['resources']
-
-                    if resources['videos'] or resources['files']:
-                        sub_contents = get_course_content(session, course_id, content['id'])
-                        fetched_contents += sub_contents
-
-                else:
-                    name = content['name']
-                    url = content['url']
-                    fetched_contents.append(f'{name}: {url}')
-
-        return fetched_contents
-
-    headers = {
-        'accept-encoding': 'gzip',
-        'accept-language': 'EN',
-        'api-version'    : '35',
-        'app-version'    : '1.4.73.2',
-        'build-number'   : '35',
-        'connection'     : 'Keep-Alive',
-        'content-type'   : 'application/json',
-        'device-details' : 'Xiaomi_Redmi 7_SDK-32',
-        'device-id'      : 'c28d3cb16bbdac01',
-        'host'           : 'api.classplusapp.com',
-        'region'         : 'IN',
-        'user-agent'     : 'Mobile-Android',
-        'webengage-luid' : '00000187-6fe4-5d41-a530-26186858be4c'
-    }
-
-    api = 'https://api.classplusapp.com/v2'
-
-try:
+        # Send message to get credentials
         reply = await m.reply(
             (
                 '**'
@@ -127,44 +127,41 @@ try:
 
                 if res.status_code == 200:
                     res = res.json()
-
                     org_id = int(res['data']['orgId'])
 
                     data = {
                         'countryExt': '91',
-                        'mobile'    : phone_no,
-                        'viaSms'    : 1,
-                        'orgId'     : org_id,
-                        'eventType' : 'login',
-                        'otpHash'   : 'j7ej6eW5VO'
+                        'mobile': phone_no,
+                        'viaSms': 1,
+                        'orgId': org_id,
+                        'eventType': 'login',
+                        'otpHash': 'j7ej6eW5VO'
                     }
-        
+
                     res = session.post(f'{api}/otp/generate', data=json.dumps(data))
 
                     if res.status_code == 200:
                         res = res.json()
-
                         session_id = res['data']['sessionId']
 
-                        reply = await message.chat.ask(
+                        reply = await m.reply(
                             (
                                 '**'
                                 'Send OTP ?'
                                 '**'
                             )
-                            ,reply_to_message_id = reply.id
                         )
 
                         if reply.text.isdigit():
                             otp = reply.text.strip()
 
                             data = {
-                                'otp'          : otp,
-                                'sessionId'    : session_id,
-                                'orgId'        : org_id,
+                                'otp': otp,
+                                'sessionId': session_id,
+                                'orgId': org_id,
                                 'fingerprintId': 'a3ee05fbde3958184f682839be4fd0f7',
-                                'countryExt'   : '91',
-                                'mobile'       : phone_no,
+                                'countryExt': '91',
+                                'mobile': phone_no,
                             }
 
                             res = session.post(f'{api}/users/verify', data=json.dumps(data))
@@ -193,39 +190,34 @@ try:
 
                             else:
                                 raise Exception('Failed to verify OTP.')
-                            
+
                         else:
                             raise Exception('Failed to validate OTP.')
-                        
+
                     else:
                         raise Exception('Failed to generate OTP.')
-                    
+
                 else:
                     raise Exception('Failed to get organization Id.')
-                
+
             else:
                 raise Exception('Failed to validate credentials.')
 
         else:
-
             token = creds.strip()
             session.headers['x-access-token'] = token
-
 
             res = session.get(f'{api}/users/details')
 
             if res.status_code == 200:
                 res = res.json()
-
                 user_id = res['data']['responseData']['user']['id']
                 logged_in = True
-            
+
             else:
                 raise Exception('Failed to get user details.')
 
-
         if logged_in:
-
             params = {
                 'userId': user_id,
                 'tabCategoryId': 3
@@ -235,7 +227,6 @@ try:
 
             if res.status_code == 200:
                 res = res.json()
-
                 courses = res['data']['responseData']['coursesData']
 
                 if courses:
@@ -245,22 +236,19 @@ try:
                         name = course['name']
                         text += f'{cnt + 1}. {name}\n'
 
-                    reply = await message.chat.ask(
+                    reply = await m.reply(
                         (
                             '**'
                             'Send index number of the course to download.\n\n'
                             f'{text}'
                             '**'
-                        ),
-                        reply_to_message_id = reply.id
+                        )
                     )
 
                     if reply.text.isdigit() and len(reply.text) <= len(courses):
 
                         selected_course_index = int(reply.text.strip())
-
                         course = courses[selected_course_index - 1]
-
                         selected_course_id = course['id']
                         selected_course_name = course['name']
 
@@ -269,16 +257,14 @@ try:
                                 '**'
                                 'Extracting course...'
                                 '**'
-                            ),
-                            quote=True
+                            )
                         )
 
-                        course_content = get_course_content(session, selected_course_id)
+                        course_content = await get_course_content(session, selected_course_id)
 
                         await loader.delete()
 
                         if course_content:
-
                             caption = (
                                 '**'
                                 'App Name : Classplus\n'
@@ -289,54 +275,46 @@ try:
                             text_file = f'assets/{get_datetime_str()}.txt'
                             open(text_file, 'w').writelines(course_content)
 
-                            await client.send_document(
-                                message.chat.id,
-                                text_file,
+                            await m.reply_document(
+                                document=text_file,
                                 caption=caption,
                                 file_name=f"{selected_course_name}.txt",
-                                reply_to_message_id=reply.id
                             )
 
                             html_file = f'assets/{get_datetime_str()}.html'
                             create_html_file(html_file, selected_course_name, course_content)
 
-                            await client.send_document(
-                                message.chat.id,
-                                html_file,
+                            await m.reply_document(
+                                document=html_file,
                                 caption=caption,
                                 file_name=f"{selected_course_name}.html",
-                                reply_to_message_id=reply.id
                             )
 
                             os.remove(text_file)
                             os.remove(html_file)
 
                         else:
-                            raise Exception('Did not found any content in course.')
+                            raise Exception('Did not find any content in the course.')
 
                     else:
                         raise Exception('Failed to validate course selection.')
 
                 else:
-                    raise Exception('Did not found any course.')
+                    raise Exception('Did not find any courses.')
 
             else:
                 raise Exception('Failed to get courses.')
-            
 
-   
-        try:
-    # Your main code that might raise exceptions
-    ...
-except Exception as error:
-    LOGGER.error(f'Error: {error}')  # Log the error
-    await m.reply(
-        (
-            '**'
-            f'Error: {error}'
-            '**'
-        ),
-        quote=True
-    )
+    except Exception as error:
+        LOGGER.error(f'Error: {error}')  # Log the error
+        await m.reply(
+            (
+                '**'
+                f'Error: {error}'
+                '**'
+            ),
+            quote=True
+        )
 
+# Start the bot
 bot.run()
