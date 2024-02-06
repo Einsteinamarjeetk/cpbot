6db1b905776c1250a3fb124e77e8cdf0efdc1dad
Vivekfhfd <132126550+Vivekfhfd@users.noreply.github.com>
2024-02-06 15:27:35 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
-auth_users = [1713924419]
-sudo_user = 1713924419
-log_channel = -1002143629860
-txt_channel = -1002049391187
+api_id = 28324139
+api_hash = "f15584fee477679e21e781e5da28c06a"
+bot_token = "6930341690:AAGB_i7VgEWrbbnpvvbtgo8-BCT8NB2Q2Hg"
+auth_users = [6843464670]
+sudo_user = 6843464670
+log_channel = -1002107741665
+txt_channel = -1002107741665
