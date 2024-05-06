fd903e09e0a37975abe431ed0147698f76f72297
Einsteinamarjeetk <156225038+Einsteinamarjeetk@users.noreply.github.com>
2024-05-06 08:24:52 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 28324139
-api_hash = "f15584fee477679e21e781e5da28c06a"
-bot_token = "6930341690:AAGB_i7VgEWrbbnpvvbtgo8-BCT8NB2Q2Hg"
-auth_users = [6843464670]
-sudo_user = 6843464670
-log_channel = -1002107741665
-txt_channel = -1002107741665
+api_id = 9438917
+api_hash = "b86192c6db595de8e8863b43648c55da"
+bot_token = "6438050573:AAH5HAfSV_5Gr_SIoP2I9CAg626ruh0-l_g"
+auth_users = [6015844922]
+sudo_user = 6015844922
+log_channel = -1002060191058
+txt_channel = -1002060191058
