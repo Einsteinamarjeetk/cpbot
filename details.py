969d8f481c41e0fc875e92cbf44db3887445fffd
Einsteinamarjeetk <156225038+Einsteinamarjeetk@users.noreply.github.com>
2024-05-10 15:01:07 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,7 +2,7 @@ import os
 
 api_id = 9438917
 api_hash = "b86192c6db595de8e8863b43648c55da"
-bot_token = "6438050573:AAH5HAfSV_5Gr_SIoP2I9CAg626ruh0-l_g"
+bot_token = "7154922307:AAFwL_1D8hEq6xo9CEKuPXeU-g7qYQKRjaI"
 auth_users = [6015844922]
 sudo_user = 6015844922
 log_channel = -1002060191058
